f991f3c29bb5eb90186bba4e99a6af020a45f13d
Adam Rajfer <adam.rajfer@gmail.com>
2023-03-02 17:38:03 +0100

    Fix running locally

--- src/devproject/project.py
+++ src/devproject/project.py
@@ -30,97 +30,60 @@ def project(args: Namespace) -> None:
         if args.rm:
             shutil.rmtree(os.path.dirname(dev_dir))
             return
-        elif args.base_image:
-            workspace_dir = f"/workspaces/{args.name}"
-            args.workdir = (
-                os.path.join(workspace_dir, args.workdir) if args.workdir
-                else workspace_dir
-            )
-            if args.workdir.endswith("/"):
-                args.workdir = args.workdir[:-1]
-            os.makedirs(dev_dir)
-            shutil.copyfile(
-                f"{get_template_dir()}/settings.json",
-                f"{dev_dir}/settings.json",
-            )
-            shutil.copyfile(
-                f"{get_template_dir()}/.bashrc", f"{dev_dir}/.bashrc"
-            )
+        workspace = f"/workspaces/{args.name}"
+        args.workdir = (
+            os.path.join(workspace, args.workdir)
+            if args.workdir else workspace
+        ).rstrip("/")
+        os.makedirs(dev_dir)
+        shutil.copy(f"{get_template_dir()}/settings.json", f"{dev_dir}/")
+        shutil.copy(f"{get_template_dir()}/.bashrc", f"{dev_dir}/")
+        shutil.copy(f"{get_template_dir()}/.profile", f"{dev_dir}/")
+        with open(f"{get_template_dir()}/Dockerfile", "r") as f_src:
             with open(f"{dev_dir}/Dockerfile", "w") as f_out:
-                f_out.write(
-                    f"FROM {args.base_image}\n" \
-                    f"WORKDIR {args.workdir}\n" \
-                    f"RUN groupadd -o --gid SRC_GID SRC_USER \\\n" \
-                    f"    && useradd --uid SRC_UID --gid SRC_GID -m SRC_USER \\\n" \
-                    f"    && apt-get update \\\n" \
-                    f"    && apt-get install -y sudo bash-completion \\\n" \
-                    f"    && echo SRC_USER ALL=\(root\) NOPASSWD:ALL >" \
-                    f" /etc/sudoers.d/SRC_USER \\\n" \
-                    f"    && chmod 0440 /etc/sudoers.d/SRC_USER\n" \
-                    f"USER SRC_USER\n" \
-                    f"COPY .bashrc /home/SRC_USER/.bashrc\n" \
-                    f"RUN pip install --upgrade pip\n" \
-                    f"RUN pip install mypy pytest\n" \
-                    f"RUN git config --global user.name" \
-                    f" {get_git_username()}\n" \
-                    f"RUN git config --global user.email" \
-                    f" {get_git_useremail()}\n" \
-                    f"RUN git config --global --add safe.directory" \
-                    f" {workspace_dir}\n" \
-                    f"RUN touch /home/SRC_USER/.gitignore\n" \
-                    f"RUN echo '.devinfo.csv' >> /home/SRC_USER/.gitignore\n" \
-                    f"RUN echo '.devcontainer/' >>" \
-                    f" /home/SRC_USER/.gitignore\n" \
-                    f"RUN echo '.devtmp/' >> /home/SRC_USER/.gitignore\n" \
-                    f"RUN echo '.hypothesis/' >> /home/SRC_USER/.gitignore\n" \
-                    f"RUN echo '**/__pycache__/' >>" \
-                    f" /home/SRC_USER/.gitignore\n" \
-                    f"RUN git config --global core.excludesfile" \
-                    f" /home/SRC_USER/.gitignore\n"
-                )
-            with open(f"{get_template_dir()}/devcontainer.json", "r") as f_src:
-                with open(f"{dev_dir}/devcontainer.json", "w") as f_out:
-                    devcontainer = json.load(f_src)
-                    devcontainer["workspaceFolder"] = args.workdir
-                    devcontainer["remoteUser"] = "SRC_USER"
-                    devcontainer["containerUser"] = "SRC_USER"
-                    cmd = f"cd {workspace_dir}; sudo rmdir -p {args.workdir}"
-                    if args.git:
-                        cmd = (
-                            f"{cmd}; git clone {args.git} .devtmp;" \
-                            f" shopt -s dotglob; mv .devtmp/* .;" \
-                            f" rm .devlock; touch .devtmp/.devlock"
-                        )
-                    else:
-                        cmd = f"{cmd}; mkdir -p {args.workdir}"
-                    if args.install_req:
-                        cmd = (
-                            f"{cmd}; pip install" \
-                            f" $(python -c 'import subprocess;" \
-                            f" print(\"\".join([f\" -r {{x}}\"" \
-                            f" for x in subprocess.getoutput(" \
-                            f"\"find {args.workdir}" \
-                            f" -maxdepth {args.req_depth}" \
-                            f" -name requirements.txt\").split()]))')"
-                        )
-                    devcontainer["postCreateCommand"] = cmd
-                    if args.mount:
-                        mount = (x.split(":") for x in args.mount)
-                        devcontainer["mounts"] = [
-                            f"source=SRC_DIR/{src},target={tgt}" \
-                            f",type=bind,consistency=cached"
-                            for src, tgt in mount
-                        ]
-                    json.dump(devcontainer, f_out, indent=4)
-            df = pd.Series(
-                dict(**vars(args), datetime=datetime.now())
-            )[COLUMNS]
-            df.to_csv(f"{dev_dir}/../.devinfo.csv")
-            df = df.to_frame().fillna("-").T
-            df.columns = [f"\033[93m{x}\033[0m" for x in COLUMNS]
-            maxcolwidths = MAX_COL_WIDTHS
-        else:
-            raise ValueError("Provide `--base-image to your command.`")
+                f_out.write(f_src.read(
+                    ).replace("SRC_IMAGE", args.base_image
+                    ).replace("SRC_WORKDIR", args.workdir
+                    ).replace("SRC_GIT_USER", get_git_username()
+                    ).replace("SRC_GIT_EMAIL", get_git_useremail()
+                    ).replace("SRC_WORKSPACE", workspace))
+        with open(f"{get_template_dir()}/devcontainer.json", "r") as f_src:
+            with open(f"{dev_dir}/devcontainer.json", "w") as f_out:
+                devcontainer = json.load(f_src)
+                devcontainer["workspaceFolder"] = args.workdir
+                devcontainer["remoteUser"] = "SRC_USER"
+                devcontainer["containerUser"] = "SRC_USER"
+                cmd = f"cd {workspace}; sudo rmdir -p {args.workdir}"
+                if args.git:
+                    cmd = (
+                        f"{cmd}; git clone {args.git} .devtmp; shopt -s " \
+                        f"dotglob; mv .devtmp/* .; rm .devlock; touch " \
+                        f".devtmp/.devlock"
+                    )
+                else:
+                    cmd = f"{cmd}; mkdir -p {args.workdir}"
+                if args.install_req:
+                    cmd = (
+                        f"{cmd}; pip install $(python -c 'import subprocess;" \
+                        f" print(\"\".join([f\" -r {{x}}\" for x in" \
+                        f" subprocess.getoutput(\"find {args.workdir}" \
+                        f" -maxdepth {args.req_depth} -name" \
+                        f" requirements.txt\").split()]))')"
+                    )
+                devcontainer["postCreateCommand"] = cmd
+                if args.mount:
+                    mount = (x.split(":") for x in args.mount)
+                    devcontainer["mounts"] = [
+                        f"source=SRC_DIR/{src},target={tgt}" \
+                        f",type=bind,consistency=cached"
+                        for src, tgt in mount
+                    ]
+                json.dump(devcontainer, f_out, indent=4)
+        df = pd.Series(dict(**vars(args), datetime=datetime.now()))[COLUMNS]
+        df.to_csv(f"{dev_dir}/../.devinfo.csv")
+        df = df.to_frame().fillna("-").T
+        df.columns = [f"\033[93m{x}\033[0m" for x in COLUMNS]
+        maxcolwidths = MAX_COL_WIDTHS
     else:
         df = [
             pd.read_csv(
--- src/devproject/run.py
+++ src/devproject/run.py
@@ -8,6 +8,7 @@ from devproject.utils import get_config, get_local_dir, get_user
 def run(args: Namespace) -> None:
     config = get_config()[args.config]
     if config["host"] == "sync":
+        # TODO: Connect with interactive host only (run with /bin/bash)
         config["host"] = subprocess.getoutput(
             f"ssh {config['gateway']} 'HOST=$(squeue -u $USER --states R" \
             f" -O nodelist --noheader | head -n 1); echo $HOST'"
@@ -17,28 +18,53 @@ def run(args: Namespace) -> None:
     remote_dir = f"{remote_src_dir}/.devprojects"
     if not os.path.exists(f"{get_local_dir()}/{args.project}"):
         raise ValueError(f"Project {args.project} does not exist.")
-    connection = f"{get_user()}@{config['host']}:" if config["gateway"] else ""
-    assert not subprocess.call(
-        f"rsync -a {get_local_dir()}/ {connection}{remote_dir}/", shell=True
-    )
-    connection = (
-        f"ssh {get_user()}@{config['host']} " if config["gateway"] else "eval '"
-    )
-    end = "" if config["gateway"] else "'"
-    assert not subprocess.call(
-        f"{connection}sed -i" \
-        f" -e 's:SRC_DIR:{remote_src_dir_bash}:g'" \
-        f" -e 's:SRC_USER:$(id -un):g'" \
-        f" -e 's:SRC_UID:$(id -u):g'" \
-        f" -e 's:SRC_GID:$(id -g):g'" \
-        f" {remote_dir}/*/.devcontainer/*{end}",
-        shell=True,
-    )
-    connection = (
-        f"vscode-remote://ssh-remote+{get_user()}@{config['host']}"
-        if config["gateway"] else ""
-    )
-    assert not subprocess.call(
-        f"code --folder-uri {connection}{remote_dir}/{args.project}",
-        shell=True,
-    )
+    if config["host"]:
+        assert not subprocess.call(
+            f"ssh {get_user()}@{config['host']}" \
+            f" mkdir -p {remote_dir}/{args.project}/.devcontainer/",
+            shell=True,
+        )
+        assert not subprocess.call(
+            f"rsync -a" \
+            f" {get_local_dir()}/{args.project}/.devcontainer/" \
+            f" {get_user()}@{config['host']}:" \
+            f"{remote_dir}/{args.project}/.devcontainer/ --delete",
+            shell=True,
+        )
+        assert not subprocess.call(
+            f"ssh {get_user()}@{config['host']} sed -i" \
+            f" -e 's:SRC_DIR:{remote_src_dir_bash}:g'" \
+            f" -e 's:SRC_USER:$(id -un):g'" \
+            f" -e 's:SRC_UID:$(id -u):g'" \
+            f" -e 's:SRC_GID:$(id -g):g'" \
+            f" {remote_dir}/{args.project}/.devcontainer/*",
+            shell=True,
+        )
+        assert not subprocess.call(
+            f"code --folder-uri" \
+            f" vscode-remote://ssh-remote+{get_user()}@{config['host']}" \
+            f"{remote_dir}/{args.project}",
+            shell=True,
+        )
+    else:
+        assert not subprocess.call(
+            f"mkdir -p {remote_dir}/{args.project}/.devcontainer/", shell=True
+        )
+        assert not subprocess.call(
+            f"rsync -a" \
+            f" {get_local_dir()}/{args.project}/.devcontainer/" \
+            f" {remote_dir}/{args.project}/.devcontainer/ --delete",
+            shell=True,
+        )
+        assert not subprocess.call(
+            f"eval 'sed -i" \
+            f" -e 's:SRC_DIR:{remote_src_dir_bash}:g'" \
+            f" -e 's:SRC_USER:$(id -un):g'" \
+            f" -e 's:SRC_UID:$(id -u):g'" \
+            f" -e 's:SRC_GID:$(id -g):g'" \
+            f" {remote_dir}/{args.project}/.devcontainer/*'",
+            shell=True,
+        )
+        assert not subprocess.call(
+            f"code --folder-uri {remote_dir}/{args.project}", shell=True
+        )
