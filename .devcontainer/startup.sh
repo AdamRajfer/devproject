9dc1b5be860f34c834874868b9cc4735f8b3a53c
Adam Rajfer <adam.rajfer@gmail.com>
2023-09-06 22:38:39 +0200

    Display startup steps

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,10 +1,21 @@
+set -e
 IMAGE=$(cat .devcontainer/devcontainer.json | grep '"image":' | sed -n 's/.*: "\([^",]*\)"\{0,1\},\{0,1\}/\1/p')
 FROM_IMAGE=$(echo $IMAGE | sed 's/\(.*\)---.*/\1/')
-docker inspect ${IMAGE} 1>/dev/null || docker build \
+USER=$(id -un)
+USER_UID=$(id -u)
+USER_GID=$(id -g)
+DOCKER_GID=$(getent group docker | cut -d: -f3)
+echo "IMAGE: ${IMAGE}"
+echo "FROM_IMAGE: ${FROM_IMAGE}"
+echo "USER: ${USER}"
+echo "USER_UID: ${USER_UID}"
+echo "USER_GID: ${USER_GID}"
+echo "DOCKER_GID: ${DOCKER_GID}"
+docker inspect ${IMAGE} &>/dev/null || docker build \
     -t ${IMAGE} \
     --build-arg FROM_IMAGE=${FROM_IMAGE} \
-    --build-arg USER=$(id -un) \
-    --build-arg USER_UID=$(id -u) \
-    --build-arg USER_GID=$(id -g) \
-    --build-arg DOCKER_GID=$(getent group docker | cut -d: -f3) \
+    --build-arg USER=${USER} \
+    --build-arg USER_UID=${USER_UID} \
+    --build-arg USER_GID=${USER_GID} \
+    --build-arg DOCKER_GID=${DOCKER_GID} \
     .devcontainer
